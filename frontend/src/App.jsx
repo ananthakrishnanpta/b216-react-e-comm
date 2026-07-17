7df4e6998cbcc26948413cbcc1f0fab469aaa45f
ananthakrishnanpta <ananthakrishnanpta@gmail.com>
2026-07-17 11:35:35 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import Navbar from './components/Navbar/Navbar';
+
+function App() {
+
+  return (
+    <>
+    <header>
+      <Navbar />
+    </header>
+      <main>
+        alksdfaslf
+      </main>
+      <footer>
+      </footer>
+    </>
+  )
+}
+
+export default App
