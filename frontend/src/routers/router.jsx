9f0f29f355048bd772763a87fe8e8a4baa8442d4
ananthakrishnanpta <ananthakrishnanpta@gmail.com>
2026-07-20 13:07:22 +0530

    Added react router

--- frontend/src/routers/router.jsx
+++ frontend/src/routers/router.jsx
@@ -25,6 +25,10 @@ const router = createBrowserRouter([
                 element : <Contact />
             }
         ]
+    },
+    {
+        path : "*",
+        element : <NotFound />
     }
 ])
 
