40735bcbd8b2d95fafc1f50d2b2b284679680a6e
ananthakrishnanpta <ananthakrishnanpta@gmail.com>
2026-08-11 13:21:27 +0530

    validation done

--- frontend/src/components/Form/Auth/SignupForm/SignupForm.jsx
+++ frontend/src/components/Form/Auth/SignupForm/SignupForm.jsx
@@ -22,12 +22,18 @@ const SignupForm = () => {
                     id="firstName"
                     name="First Name"
                     placeholder="Enter your first name"
+                    register = {
+                        register('firstName')
+                    }
+                    error = {errors.firstName}
                 />
 
                 <BasicInput
                     id="lastName"
                     name="Last Name"
                     placeholder="Enter your last name"
+                    register={register('lastName')}
+                    error={errors.lastName}
                 />
 
                 <BasicInput
@@ -35,6 +41,15 @@ const SignupForm = () => {
                     name="Email"
                     type="email"
                     placeholder="Enter your email"
+                    register={
+                        register('email', {
+                            required: "Email is required",
+                            pattern: {
+                                value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                                message: "Enter a valid email address"
+                            }
+                        })
+                    }
                 />
 
                 {/* Password */}
@@ -43,34 +58,37 @@ const SignupForm = () => {
                     name="Password"
                     placeholder="Enter your password"
                     register={
-                        register("password",
-                        {
+                        register("password", {
                             required: "Password is required",
                             minLength: {
                                 value: 8,
-                                message: "Password must be atleast 8 characters."
+                                message: "Password must be at least 8 characters."
+                            },
+                            pattern: {
+                                value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&]).+$/,
+                                message: "Password must contain uppercase, lowercase, number and special character."
                             }
                         })
                     }
-                    error = {errors.password}
+                    error={errors.password}
                 />
 
                 <PasswordInput
                     id="confirmPassword"
                     name="Confirm Password"
                     placeholder="Confirm your password"
-                    register = {
+                    register={
                         register(
                             "confirmPassword", {
-                                required : "Please confirm your password.",
-                                validate: (value) => 
-                                    value === password || "Passwords do not match"
-                            })}
-                            error = {errors.confirmPassword}
+                            required: "Please confirm your password.",
+                            validate: (value) =>
+                                value === password || "Passwords do not match"
+                        })}
+                    error={errors.confirmPassword}
                 />
 
                 <button
-                    type="button"
+                    type="submit"
                     className="btn btn-primary w-100 mt-2"
                 >
                     Sign Up
